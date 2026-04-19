770e808922887eeababa5fa1be83ddefaeda393b
ilyasvvv <ilyasvaliyev0@gmail.com>
2026-04-18 21:24:41 -0400

    Auto-commit: main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./utils/auth";
+import { SearchProvider } from "./utils/SearchContext";
+import SearchOverlay from "./components/SearchOverlay";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <SearchProvider>
+          <App />
+          <SearchOverlay />
+        </SearchProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
